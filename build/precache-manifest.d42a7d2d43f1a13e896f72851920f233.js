184b428e89d58a12d1b9513ae1a4d0c735e019e0
Arseniy <mrsinkrait@gmail.com>
2021-02-17 17:10:01 +0600

    build #1

---
+++ build/precache-manifest.d42a7d2d43f1a13e896f72851920f233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00520108a0c4266deb210facaa070efb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b99873f0330d82fd38f",
+    "url": "/static/css/main.d2f6c4da.chunk.css"
+  },
+  {
+    "revision": "2ce9cbd632c899942a88",
+    "url": "/static/js/2.37f72f0e.chunk.js"
+  },
+  {
+    "revision": "ef6360bd81444bdffc77a2e6cbe56b2c",
+    "url": "/static/js/2.37f72f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b99873f0330d82fd38f",
+    "url": "/static/js/main.1afd3c51.chunk.js"
+  },
+  {
+    "revision": "6e71844ecf1617a8c0e1",
+    "url": "/static/js/runtime-main.4216a829.js"
+  },
+  {
+    "revision": "107181e44ae91687de56b91f956735c0",
+    "url": "/static/media/close.107181e4.svg"
+  }
+]);
